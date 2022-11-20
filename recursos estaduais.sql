a310fd9ac7ad09c59e4e484431ab1e202ad80070
João Vitor Ferreira <jvferreira.20@gmail.com>
2022-11-20 16:43:26 -0300

    Initial Commit

---
+++ recursos estaduais.sql
@@ -0,0 +1,11 @@
+-- Create table recursos_estaduais
+CREATE TABLE recursos_estaduais (
+  uf VARCHAR(10),
+  loa VARCHAR(50),
+  empenhado VARCHAR(50),
+  liquidado VARCHAR(50),
+  pago VARCHAR(50),
+  dividas VARCHAR(50),
+  extra VARCHAR(10),
+);
+
